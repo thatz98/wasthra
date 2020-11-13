855b29aa87b57c998fb34d3619456bab76a930f5
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-11-13 16:16:21 +0530

    Wishlist UI (initial)

---
+++ views/user/wish_list.php
@@ -0,0 +1,154 @@
+<?php require 'views/header.php'; ?>
+
+
+<div class="small-container">
+        <h2 class="title">My Wishlist</h2>
+            <div class="row">
+               
+                <div class="col-4">
+                <div class="content">
+                        <div class="content-overlay"></div>
+                        <img src="<?php ; ?>public/images/products/product_img_2.jpg" alt="item1" />
+                        
+                    <div class="content-details fadeIn-bottom"> 
+                        <div class="options">
+                            <div class="text">
+                                <a href="#">View</a><br><br>
+                            </div>
+                                <a href="#"><i class="fa fa-2x fa-heart"></i></a><a href="#"><i class="fa fa-2x fa-cart-plus"></i></a>
+                        </div>
+                    </div>
+
+                    <div>
+                      <h4>Square Lines</h4>
+                      <div class="ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                      </div>
+                    <p>LKR 880.00 </p>
+
+                    </div>
+                    
+                </div>                
+                </div>
+
+              
+                <div class="col-4">
+                <div class="content">
+                        <div class="content-overlay"></div>
+                        <img src="<?php ; ?>public/images/products/product_img_8.jpg" alt="item1" />
+                        
+                    <div class="content-details fadeIn-bottom"> 
+                        <div class="options">
+                            <div class="text">
+                                <a href="#">View</a><br><br>
+                            </div>
+                                <a href="#"><i class="fa fa-2x fa-heart"></i></a><a href="#"><i class="fa fa-2x fa-cart-plus"></i></a>
+                        </div>
+                    </div>
+
+                    <div>
+                      <h4>Metro D-Mix</h4>
+                      <div class="ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                        <i class="fa fa-star-o"></i>
+                      </div>
+                    <p>LKR 700.00 </p>
+
+                    </div>
+                    
+                </div>                
+                </div>
+
+
+                <div class="col-4">
+                <div class="content">
+                        <div class="content-overlay"></div>
+                        <img src="<?php ; ?>public/images/products/product_img_7.jpg" alt="item1" />
+                        
+                    <div class="content-details fadeIn-bottom"> 
+                        <div class="options">
+                            <div class="text">
+                                <a href="#">View</a><br><br>
+                            </div>
+                                <a href="#"><i class="fa fa-2x fa-heart"></i></a><a href="#"><i class="fa fa-2x fa-cart-plus"></i></a>
+                        </div>
+                    </div>
+
+                    <div>
+                      <h4>Karma Recycle</h4>
+                      <div class="ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                      </div>
+                    <p>LKR 800.00 </p>
+
+                    </div>
+                    
+                </div>                
+                </div>
+
+
+                <div class="col-4">
+                <div class="content">
+                        <div class="content-overlay"></div>
+                        <img src="<?php ; ?>public/images/products/product_img_1.jpg" alt="item1" />
+                        
+                    <div class="content-details fadeIn-bottom"> 
+                        <div class="options">
+                            <div class="text">
+                                <a href="#">View</a><br><br>
+                            </div>
+                                <a href="#"><i class="fa fa-2x fa-heart"></i></a><a href="#"><i class="fa fa-2x fa-cart-plus"></i></a>
+                        </div>
+                    </div>
+
+                    <div>
+                      <h4>Moving Units Soulcal</h4>
+                      <div class="ratings">
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star"></i>
+                        <i class="fa fa-star-o"></i>
+                      </div>
+                    <p>LKR 900.00 </p>
+
+                    </div>
+                    
+                </div>                
+                </div>
+                
+            </div>
+
+
+
+ </div>
+
+
+
+
+<?php require 'views/footer.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
