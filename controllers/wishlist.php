ae542b08ea94e1cf2336d8742da834289356cdac
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-11-15 11:11:53 +0530

    Add Index _to_wishlist

--- controllers/wishlist.php
+++ controllers/wishlist.php
@@ -9,9 +9,29 @@ class Wishlist extends Controller{
     }
 
     function index(){
-    	
+     
+        $this->view->qtyList =  $this->model->getAllDetails();
+        $this->view->sizeList =  $this->model->getSizes();
+        $this->view->imageList =  $this->model->getImages();
+        $this->view->colorList =  $this->model->getColors();
+        
+        $this->view->breadcumb = '<a href="'.URL.'">Home</a> <i class="fas fa-angle-right"></i> Wishlist';
     	$this->view->render('user/wish_list');
     }
+
+    function addToWishlist($id){
+
+        $this->model->create($id);
+        header('location: '.URL.'');
+
+   }
+
+   function removeFromWishlist($id){
+
+    $this->model->delete($id);
+    header('location: '.URL.'wishlist');
+
+}
 
   
 }
