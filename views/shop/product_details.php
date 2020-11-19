89167482d934ed8044987815f9ce57ab6c35393e
thatz98 <thatz98@users.noreply.github.com>
2020-11-19 11:23:27 +0530

    Update product_details.php

--- views/shop/product_details.php
+++ views/shop/product_details.php
@@ -27,21 +27,21 @@
                 }
                 if ($this->product[0]['name'] == 'Gents') : ?>
                 <div class="gallery-col">
-                    <img src="<?php echo URL; ?>public/images/size_charts/gents" id="sizeC"
+                    <img src="<?php echo URL; ?>public/images/size_charts/gents.jpg" id="sizeC"
                         onclick="swapViewImage('sizeC')" width="100%" class="view-gallery-img">
                 </div>
                 <?php else : if ($this->product[0]['name'] == 'Ladies') : ?>
                 <div class="gallery-col">
-                    <img src="<?php echo URL; ?>public/images/size_charts/ladies" id="sizeCL"
+                    <img src="<?php echo URL; ?>public/images/size_charts/ladies.jpg" id="sizeCL"
                         onclick="swapViewImage('sizeCL')" width="100%" class="view-gallery-img">
                 </div>
                 <?php else : ?>
                 <div class="gallery-col">
-                    <img src="<?php echo URL; ?>public/images/size_charts/gents" id="sizeC"
+                    <img src="<?php echo URL; ?>public/images/size_charts/gents.jpg" id="sizeC"
                         onclick="swapViewImage('sizeC')" width="100%" class="view-gallery-img">
                 </div>
                 <div class="gallery-col">
-                    <img src="<?php echo URL; ?>public/images/size_charts/ladies" id="sizeCL"
+                    <img src="<?php echo URL; ?>public/images/size_charts/ladies.jpg" id="sizeCL"
                         onclick="swapViewImage('sizeCL')" width="100%" class="view-gallery-img">
                 </div>
                 <?php endif;
