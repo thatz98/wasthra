5b5b85f0bca8e723a7e03480f4ff4be9d4248da4
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-17 00:09:25 +0530

    redirect path fix

--- controllers/orders.php
+++ controllers/orders.php
@@ -112,7 +112,7 @@ class Orders extends Controller{
 
         $this->model->update($data);
 
-        header('location: ' . URL . '');
+        header('location: ' . URL . 'orders/orderDashboard');
     }
 
 }
