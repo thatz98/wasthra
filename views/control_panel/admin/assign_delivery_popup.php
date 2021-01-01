1cb4633530fb5da51f41d009c17cdb89fda2aaef
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2021-01-01 18:38:17 +0530

    rediretion fix

--- views/control_panel/admin/assign_delivery_popup.php
+++ views/control_panel/admin/assign_delivery_popup.php
@@ -3,7 +3,7 @@
     <div class="popup">
         <a href="#" class="close-btn"><i class="fa fa-times-circle"></i></a>
         <div class="row">
-            <form action="<?php echo URL; ?>order/updateOrderStatus" id="AssignDeliverForm" method="post">
+            <form action="<?php echo URL; ?>orders/create" id="AssignDeliverForm" method="post">
                 <div class="row" style="margin-top:30px;">
                     <div class="col-2">
                         <div class="">
@@ -21,6 +21,7 @@
                     </div>
                 </div>
                 <div class="row">
+                <input type="text" value="<?php echo  $this->orderItems[0][0]; ?>" name="order_id" hidden>
                     <button type="submit" class="btn">Assign</button>
                 </div>
                 <br>
