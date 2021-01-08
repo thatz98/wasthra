ed7af9dc353eebb6336999d77163a08c45d9c683
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-01-08 11:17:49 +0530

    edit pricecategory validation(initial)

--- views/control_panel/owner/edit_price_categories.php
+++ views/control_panel/owner/edit_price_categories.php
@@ -85,7 +85,7 @@
 
 
 <script type="text/javascript" src="<?php echo URL ?>public/js/form_validation.js"></script>
-<script type="text/javascript" src="<?php echo URL ?>util/form/price_category_form_validation.js"></script>
+<script type="text/javascript" src="<?php echo URL ?>util/form/edit_price_category_form_validation.js"></script>
 
 <?php require 'views/footer.php'; ?>
 
