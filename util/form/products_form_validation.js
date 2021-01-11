d3764e685ae4a19c39cf4978bd81eeee623093d5
Iranga Mudalige <irangapriyam@gmail.com>
2021-01-11 15:16:05 +0530

    dropdown validations added

--- util/form/products_form_validation.js
+++ util/form/products_form_validation.js
@@ -3,6 +3,7 @@ const productName = document.getElementById('product_name');
 const quantity = document.getElementById('quantity');
 const colors = document.getElementById('color');
 const description = document.getElementById('description');
+const image = document.getElementById('image');
 
 const editform = document.getElementById('addFrom');
 
@@ -16,7 +17,14 @@ editform.addEventListener('submit', function(event) {
         validateColors() &&
         validateQuantity() &&
         validateDescription() &&
-        validateSingleSize()
+        validateSingleSize() &&
+        validateIsPublished() &&
+        validateIsNew() &&
+        validateIsFeatured() &&
+        validatePriceCategory() &&
+        validateProductCategory() 
+        // validateImage()
+
     ) {
       editform.submit();
     }
@@ -72,4 +80,42 @@ function validateSingleSize() {
   document.getElementById('XL').checked==true) return true;
       setinvalidWithoutColor(document.getElementById('size-field'),'Must be selected');
       return false;
+}
+
+function validateIsPublished(){
+  if(document.getElementById('is_published').value!="0") return true;
+  setinvalidWithoutColor(document.getElementById('is_published'),'Must be selected');
+      return false;
+}
+
+function validateIsNew(){
+  if(document.getElementById('is_new').value!="0") return true;
+  setinvalidWithoutColor(document.getElementById('is_published'),'Must be selected');
+      return false;
+}
+
+function validateIsFeatured(){
+  if(document.getElementById('is_featured').value!="0") return true;
+  setinvalidWithoutColor(document.getElementById('is_published'),'Must be selected');
+      return false;
+}
+
+function validatePriceCategory(){
+  if(document.getElementById('price_category').value!="0") return true;
+  setinvalidWithoutColor(document.getElementById('price_category'),'Must be selected');
+      return false;
+}
+
+function validateProductCategory(){
+  if(document.getElementById('category').value!="0") return true;
+  setinvalidWithoutColor(document.getElementById('category'),'Must be selected');
+      return false;
+}
+
+function validateImage(){
+  
+  if (checkIfEmpty(image)) return;
+  // if(document.getElementById('image').value!=NULL) return true;
+  // setinvalidWithoutColor(document.getElementById('image'),'Must be selected');
+       return false;
 }
