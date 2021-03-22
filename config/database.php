7151e9f20e777f7897c41b90b8175f2656032c0f
Thatz Weerakoon <59426115+thatz98@users.noreply.github.com>
2021-03-22 12:54:26 +0530

    Revert "path chanhges"
    This reverts commit 5a7d37b15183705a4cf4bbae5a06b3fa5916a9f9.

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ define('DB_HOST','localhost');
 define('DB_NAME','db_wasthra');
 //define('DB_NAME','hloy');
 define('DB_USER','root');
-define('DB_PASS','wasmysql2021');
+define('DB_PASS','');
 
 
  
