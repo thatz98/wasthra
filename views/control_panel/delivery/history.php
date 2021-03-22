42580ad1fab02eb19e11f51f184654bb631c6e0f
Darshana1206 <56540757+Darshana1206@users.noreply.github.com>
2021-03-22 12:21:40 +0530

    remove hardcoded lines

--- views/control_panel/delivery/history.php
+++ views/control_panel/delivery/history.php
@@ -28,43 +28,9 @@
                            <td><a href="<?php echo URL;?>orders/historyDetails" class="btn table-btn">View Details</a></td>
                        </tr>
                        <?php endforeach;} ?>
-                       <!-- <tr>
-                           
-                           <td class="order-details"><h4>ORDER ID: ORD0001</h4>
-                            <h5>Date: 10/05/2020</h5>
-                            <h5>Total Price: LKR 2400.00</h5></td>
-                            <td class="order-messages">
-                              <div class="oder-status">
-                                <h5>Order Type: </h5>
-                                <h5 style="color: #04CBE0"> Delivery</h5><br>
-                              </div>
-                              <div class="oder-date">
-                                <h5>Delivered Date: </h5>
-                                <h5 style="color: #04CBE0"> 24/05/2020</h5>
-                              </div>
-                            </td>
-                           <td><a href="<?php echo URL;?>orders/historyDetails" class="btn table-btn">View Details</a></td>
-                       </tr>
-                       <tr>
-                           
-                           <td class="order-details"><h4>ORDER ID: ORD0001</h4>
-                            <h5>Date: 10/05/2020</h5>
-                            <h5>Total Price: LKR 2400.00</h5></td>
-                            <td class="order-messages">
-                              <div class="oder-status">
-                                <h5>Order Type: </h5>
-                                <h5 style="color: #04CBE0"> Delivery</h5><br>
-                              </div>
-                              <div class="oder-date">
-                                <h5>Delivered Date: </h5>
-                                <h5 style="color: #04CBE0"> 24/05/2020</h5>
-                              </div>
-                            </td>
-                           <td><a href="<?php echo URL;?>orders/historyDetails" class="btn table-btn">View Details</a></td>
-                       </tr> -->
                        
                    </table>     
-                    </div>
+         </div>
 
 </div>
 
