c533f1b0eba511665abd57962047102aa2a4d488
Iranga Mudalige <irangapriyam@gmail.com>
2021-03-29 18:06:06 +0530

    Update products.php

--- views/control_panel/admin/products.php
+++ views/control_panel/admin/products.php
@@ -4,7 +4,9 @@
     <div class="row">
         <h2 class="title title-min">Products</h2>
     </div>
-
+    <div class="row-right">
+        <a href="<?php echo URL ?>inventory" class="btn">Manage Inventory</a>
+    </div>
     <div class="row">
         <div class="col-3 fit-size">
             <div class="min-card primary">
